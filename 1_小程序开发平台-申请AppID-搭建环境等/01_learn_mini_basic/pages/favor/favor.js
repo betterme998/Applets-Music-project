1d97926c7480bd1ea2f98239486f1557cbb2f468
betterme998 <1248715031@qq.com>
2022-10-27 14:16:05 +0800

    绑定数据等案例

--- 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/favor/favor.js
+++ 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/favor/favor.js
@@ -1,66 +1,27 @@
 // pages/favor/favor.js
+// 实例化：页面实例
 Page({
+  data: {
+    // 1.案例一：数据绑定mustache语法
+    message:"hello world",
+    // 2.案例二：列表数据
+    movies:["少年派","大话西游","星际穿越","独行月球"],
+    // 3.案例三：计数器案例
+    counter:0
+  },
+  // 监听的事件方法
+  increment(){
+    // 修改data中的数据，并不会引起页面刷新，在小程序中不会，在react中也不会
+    // this.data.counter += 1
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
+    // 修改data，并且希望页面重新渲染，这里必须使用this.setData()
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  decrement(){
+    this.setData({
+      counter:this.data.counter-1
+    })
+  }
 })
