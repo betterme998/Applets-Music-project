96ea0e3749cf390e3e032953f2d71bfcb95a82b8
betterme998 <betterme998@163.com>
2023-02-14 15:49:49 +0800

    WXS语法和练习

---
+++ 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/05_learn_wxs/index.js
@@ -0,0 +1,13 @@
+// pages/05_learn_wxs/index.js
+Page({
+  data:{
+    books:[
+      { id:111, name: "代码大全", price: 98 },
+      { id:112, name: "你不知道的JS", price: 87 },
+      { id:113, name: "JS高级程序设计", price: 76 }
+    ],
+    playCount:22024523,
+    duration:255,
+    currentTime:65
+  }
+})
--- 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/index/index.js
+++ 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/index/index.js
@@ -7,8 +7,9 @@ Page({
     pages: [
       {name:"01_初体验",path:"/pages/01_register_page/index"},
       {name:"02_常见组件",path:"/pages/02_common_cpns/index"},
-      {name:"02_学习WXSS",path:"/pages/03_learn_wxss/index"},
-      {name:"02_学习WXML",path:"/pages/04_learn_wxml/index"}
+      {name:"03_学习WXSS",path:"/pages/03_learn_wxss/index"},
+      {name:"04_学习WXML",path:"/pages/04_learn_wxml/index"},
+      {name:"05_学习WXS",path:"/pages/05_learn_wxs/index"}
     ]
   },
   onBtnClick(event) {
