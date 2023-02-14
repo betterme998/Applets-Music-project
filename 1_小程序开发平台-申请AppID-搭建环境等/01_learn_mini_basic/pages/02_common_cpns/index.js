19175124fd8df22f16a9e2d22fd8d2ef90c36909
betterme998 <betterme998@163.com>
2023-02-14 11:09:21 +0800

    监听滚动-input双向数据绑定

--- 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/02_common_cpns/index.js
+++ 1_小程序开发平台-申请AppID-搭建环境等/01_learn_mini_basic/pages/02_common_cpns/index.js
@@ -1,7 +1,9 @@
 // pages/02_common_cpns/index.js
 Page({
   data:{
-    message:"你好啊，李银河"
+    message:"你好啊，李银河",
+    chooseImageUrl:"",
+    viewColors: ["red","blue","green","orange","purple","yellow"]
   },
   getUserInfo(event){
     // 调用API，获取用户的信息
@@ -21,5 +23,24 @@ Page({
   },
   onViewClick() {
     console.log("onViewClick");
+  },
+  onChooseImage(){
+    wx.chooseMedia({
+      mediaType: "image",
+    }).then(res => {
+      console.log(res);
+      const imagePath = res.tempFiles[0].tempFilePath
+      this.setData({chooseImageUrl: imagePath})
+    })
+  },
+  // 监听scroll-view滚动
+  onScrollToUpper(){
+    console.log("滚动到最顶部/左边");
+  },
+  onScrollToLower(){
+    console.log("滚动到最底部/右边");
+  },
+  onScroll(event){
+    console.log("scrollView发生了滚动",event);
   }
 })
