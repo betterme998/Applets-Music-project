99dd2f0d762be001b9e14e0f679cefed9142964e
betterme998 <betterme998@163.com>
2023-05-25 17:34:19 +0800

    优化歌单滚动

--- Applets-Music-project/pages/detail-song/detail-song.js
+++ Applets-Music-project/pages/detail-song/detail-song.js
@@ -3,7 +3,11 @@ import recommendStore from "../../store/recommendStore"
 import rankingStore, { rankingsIds } from "../../store/rankingStore"
 import { getPlaylistDetail } from "../../services/music"
 import playerStore from "../../store/playerStore"
+import { debounce } from "../../utils/debounce"
 const app = getApp()
+let startY = 0;//起始坐标
+let moveY = 0;//移动坐标
+let moveDistance = 0; //手指移动距离
 Page({
     data: {
         type:"ranking",
@@ -15,7 +19,8 @@ Page({
         bodyHeight:0,
         triggered:false,
         headerHeight:225,
-        scrollTop:0
+        scrollTop:0,
+        pullDown:true
     },
     onLoad(options) {
         // 1.确定获取数据的类型
@@ -42,16 +47,13 @@ Page({
         // 获取高度
         this.getNavTabHeight()
     },
-    // onPullDownRefresh(){
-    //     // wx.startPullDownRefresh()
-    //     wx.setBackgroundColor({
-    //         backgroundColor: this.data.bgColor[3],
-    //         backgroundColorTop: this.data.bgColor[3], // 顶部窗口的背景色为白色
-    //         backgroundColorBottom: this.data.bgColor[3] // 底部窗口的背景色为白色
-    //     })
-    //     wx.stopPullDownRefresh()
-    //     console.log('下拉');
-    // },
+    onPageScroll(event){
+        let that = this
+        this.PageScrollFn(event,that)
+    },
+    onPullDownRefresh(){
+        wx.stopPullDownRefresh()
+    },
     async fetchMenuSongInfo() {
         const res =  await getPlaylistDetail(this.data.id)
         console.log(res);
@@ -69,6 +71,11 @@ Page({
         this.setData({
             bgColor
         }),
+        wx.setBackgroundColor({
+            backgroundColor: this.data.bgColor[3],
+            backgroundColorTop: this.data.bgColor[3], // 顶部窗口的背景色为白色
+            backgroundColorBottom: this.data.bgColor[3], // 底部窗口的背景色为白色
+        })
         this.getHeaderHeight()
     },
     onNavBackTap(){
@@ -79,21 +86,50 @@ Page({
             triggered:false
         })
     },
-    binddragend(event) {
-        console.log(event);
-        if (event.detail.scrollTop < this.data.headerHeight * .2) {
-            this.setData({
-                scrollTop:0
-            })
-        }else if (event.detail.scrollTop<this.data.headerHeight) {
-            this.setData({
-                scrollTop:this.data.headerHeight
-            })
-        }
-    },
-    binddragging(event){
-        // console.log(event);
-    },
+    // binddragend(event) {
+    //     console.log(event);
+    //     if (event.detail.scrollTop < this.data.headerHeight * .2) {
+    //         this.setData({
+    //             scrollTop:0
+    //         })
+    //     }else if (event.detail.scrollTop<this.data.headerHeight) {
+    //         this.setData({
+    //             scrollTop:this.data.headerHeight
+    //         })
+    //     }
+    // },
+    // binddragging(event){
+        
+    // },
+    // handleTouchStart(e) {
+    //     let query = wx.createSelectorQuery();
+    //     query.select('.scrollViewCon').boundingClientRect(res =>{
+    //        let aaa = this.selectComponent('.scrollViewCon')
+    //        console.log(aaa);
+    //     }).exec();
+    //     // this.scrollView = this.selectComponent('.scrollViewCon')
+    //     this.setData({
+    //         coverTranstion:''
+    //     })
+    //     // 手指起始坐标
+    //     startY = e.touches[0].clientY;
+    //     console.log(startY);
+    // },
+    // handleTouchMove(e) {
+    //     moveY = e.touches[0].clientY;
+    //     moveDistance = moveY - startY
+    //     if (moveDistance<=0) return
+    //     if ((this.data.scrollTop === 0)&&this.data.pullDown) {
+    //         this.data.pullDown = false
+    //         wx.startPullDownRefresh()
+    //         console.log('下拉');
+    //     }
+
+    // },
+    // handleTouchEnd(e) {
+    //     this.data.pullDown = true
+    //     wx.stopPullDownRefresh()
+    // },
     // 获取nav+tab高度
     getNavTabHeight() {
         let query = wx.createSelectorQuery();
@@ -113,6 +149,7 @@ Page({
         let query = wx.createSelectorQuery();
         wx.nextTick(()=>{
             query.select('#header').boundingClientRect(res =>{
+                console.log(res);
                 let headerHeight = res?.height
                 this.setData({
                     headerHeight
@@ -120,6 +157,22 @@ Page({
             }).exec();
         })
     },
+    // 滚动
+    PageScrollFn:debounce((event,that) =>{
+        console.log(event.scrollTop,that.data.headerHeight);
+
+        if ( event.scrollTop < that.data.headerHeight*.3) {
+            wx.pageScrollTo({
+                scrollTop: 0,
+                duration: 300
+            })
+        }else if (event.scrollTop < that.data.headerHeight) {
+            wx.pageScrollTo({
+                scrollTop: that.data.headerHeight,
+                duration: 300
+            })
+        }
+    },100),
 
     // ==============store共享数据=============
 
